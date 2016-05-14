615c33c8772f88c1e0bd37aafc394d0004fd8596
Connor Douthat <connor@steelcastleweb.com>
2016-05-13 20:50:03 -0700

    Add blank texture image while loading

--- src/client/js/pipeline/models.js
+++ src/client/js/pipeline/models.js
@@ -35,16 +35,20 @@ function inflateModel(orig)
 
 function loadTexture(id)
 {
-	var img = new Image();
 	var tex = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, tex);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([255, 255, 255, 255]));
+
+	var img = new Image();
 	img.onload = function(){
 		gl.bindTexture(gl.TEXTURE_2D, tex);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
 		gl.generateMipmap(gl.TEXTURE_2D);
 	};
 	img.src = "tex/" + id + ".png";
+
 	return tex;
 }
 
