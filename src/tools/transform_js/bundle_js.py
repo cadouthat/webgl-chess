72935ebc4f11d9e753074b9eecedb7e40a959451
Connor Douthat <connor@steelcastleweb.com>
2016-05-15 00:25:21 -0700

    Copyright text

--- src/tools/transform_js/bundle_js.py
+++ src/tools/transform_js/bundle_js.py
@@ -7,6 +7,8 @@ if len(sys.argv) < 3:
 
 source = sys.argv[1]
 with open(sys.argv[2], "w") as dest:
+	dest.write("// Copyright 2016 Connor Douthat\n")
+	dest.write("// https://github.com/cadouthat/webgl-chess\n\n")
 	for path in os.listdir(source):
 		with open(source + path, "r") as content:
 			dest.write(content.read())
