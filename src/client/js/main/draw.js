7a83021d803b53c7fe28553cdcd31bff64979c9f
Connor Douthat <connor@steelcastleweb.com>
2016-05-15 11:50:10 -0700

    Adding chess board

--- src/client/js/main/draw.js
+++ src/client/js/main/draw.js
@@ -7,6 +7,10 @@ function draw(msTime)
 	gl.uniform3fv(main_shader.uniform.eye, cam.getEye());
 	gl.uniformMatrix4fv(main_shader.uniform.mvp, false, mvp.getMvp());
 
+	//Draw chess board
+	gl.bindTexture(gl.TEXTURE_2D, tex_mixed_marble);
+	drawModel(mdl_board);
+
 	//Progress game time
 	update(msTime);
 
--- src/client/js/main/loading.js
+++ src/client/js/main/loading.js
@@ -1,6 +1,7 @@
 function loadModels()
 {
 	//Inflate models
+	mdl_board = inflateModel(src_board);
 	mdl_king = inflateModel(src_king);
 	mdl_queen = inflateModel(src_queen);
 	mdl_bishop = inflateModel(src_bishop);
@@ -11,4 +12,5 @@ function loadModels()
 	//Load textures
 	tex_white_marble = loadTexture("white_marble");
 	tex_black_marble = loadTexture("black_marble");
+	tex_mixed_marble = loadTexture("mixed_marble");
 }
