e70b14b3abbea5328ff9545d1f8807c76d47a966
Connor Douthat <connor@steelcastleweb.com>
2016-06-24 01:21:30 -0700

    Easing animations

--- src/client/js/graphics/ChessRenderer.js
+++ src/client/js/graphics/ChessRenderer.js
@@ -23,6 +23,7 @@ function ChessRenderer(game)
 		});
 	};
 
+	//Find a piece representing a given game piece object
 	this._findByGamePiece = function(gamePiece)
 	{
 		for(var i = 0; i < this.pieces.length; i++)
@@ -66,6 +67,7 @@ function ChessRenderer(game)
 			piece.animTimer += span;
 			if(piece.birth)
 			{
+				//Fall from the sky
 				var p = piece.animTimer / this.BIRTH_DURATION;
 				if(p >= 1)
 				{
@@ -76,6 +78,7 @@ function ChessRenderer(game)
 			}
 			else if(piece.death)
 			{
+				//Rise to the sky
 				var p = piece.animTimer / this.BIRTH_DURATION;
 				if(p >= 1)
 				{
@@ -92,15 +95,40 @@ function ChessRenderer(game)
 				{
 					p = 1;
 				}
-				piece.worldPosition.x = startPosition.x + (endPosition.x - startPosition.x) * p;
-				piece.worldPosition.z = startPosition.z + (endPosition.z - startPosition.z) * p;
 
+				//Add a vertical arc for knights and castling kings
 				if(piece.gamePiece.constructor == ChessKnight ||
 					(piece.gamePiece.constructor == ChessKing && Math.abs(piece.gamePosition[0] - piece.gamePiece.position[0]) == 2))
 				{
+					piece.worldPosition.x = startPosition.x + (endPosition.x - startPosition.x) * p;
+					piece.worldPosition.z = startPosition.z + (endPosition.z - startPosition.z) * p;
 					piece.worldPosition.y = this.MOVE_HEIGHT * (1 - Math.pow(p * 2 - 1, 2));
 				}
+				else
+				{
+					//Add a slight ease to the start and end speed
+					var p_ease = p;
+					var t = piece.animTimer;
+					t /= (this.MOVE_DURATION / 2);
+					if(t < 1)
+					{
+						p_ease = Math.pow(t, 3) / 2;
+					}
+					else
+					{
+						t -= 2;
+						p_ease = Math.pow(t, 3) / 2 + 1;
+					}
+					if(p >= 1)
+					{
+						p_ease = 1;
+					}
+					//Interpolate start to end motion
+					piece.worldPosition.x = startPosition.x + (endPosition.x - startPosition.x) * p_ease;
+					piece.worldPosition.z = startPosition.z + (endPosition.z - startPosition.z) * p_ease;
+				}
 
+				//Update cached position after animation ends
 				if(p >= 1)
 				{
 					piece.gamePosition = [piece.gamePiece.position[0],
@@ -118,6 +146,7 @@ function ChessRenderer(game)
 		for(var i = 0; i < this.pieces.length;)
 		{
 			var piece = this.pieces[i];
+			//Wait until death animation is complete
 			if(piece.death && piece.animTimer >= this.BIRTH_DURATION)
 			{
 				this.pieces.splice(i, 1);
@@ -126,6 +155,7 @@ function ChessRenderer(game)
 		}
 	};
 
+	//Find a piece with a matching game position
 	this.pieceAt = function(gamePosition)
 	{
 		for(var i = 0; i < this.pieces.length; i++)
